bad6f840b328b9b4b929874b3b22dcd1e643569a
dliubich <detmir-group\DLiubich>
2025-03-02 20:56:04 +0300

    infinityScroll: добавил infinityScroll и дебаунсер

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import { useCallback, useState } from 'react';
 import './App.css';
-import Input from './components/input';
+import Input from './components/Input';
 import ReposList from './components/ReposList';
-import { useDispatch } from './store/hooks';
-import { fetchRepos } from './store/slices/reposSlice';
+import { useDispatch, useSelector } from './store/hooks';
+import { fetchRepos, resetRepos } from './store/slices/reposSlice';
+import { debounce } from './helpers';
+import ErrorBoundary from './components/ErrorBoundaries';
 
-
-const App = () => {
-  const [userName, setUserName] = useState<string>('')
+const App: React.FC = () => {
+  const [userName, setUserName] = useState<string>('');
+  const firstPage = useSelector(state => state.repos.page)
+  const [page, setPage] = useState<number>(firstPage)
   const dispatch = useDispatch();
 
+  const fetchReposDebounced = useCallback(
+    debounce((userName: string) => {
+      dispatch(resetRepos());
+      dispatch(fetchRepos({ userName, page }));
+    }, 500),
+    [dispatch]
+  );
+
   const handleInput = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setUserName(event.target.value);
-    if (event.target.value === 'anton') {
-      dispatch(fetchRepos(event.target.value));
-    }
-  }, [dispatch]);
+    const newUserName = event.target.value;
+    setUserName(newUserName);
+    setPage(1);
+    fetchReposDebounced(newUserName);
+  }, [fetchReposDebounced]);
 
   return (
     <>
-      <Input handleInput={handleInput} name={userName} />
-      <ReposList />
+      <ErrorBoundary>
+        <Input handleInput={handleInput} name={userName} />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <ReposList userName={userName} setPage={setPage} />
+      </ErrorBoundary>
     </>
   )
 }
--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -4,15 +4,15 @@ type InputProps = {
 	handleInput: (event: React.ChangeEvent<HTMLInputElement>) => void;
 	name: string;
 }
-const Input = ({ handleInput, name }: InputProps) => {
+const Input: React.FC<InputProps> = ({ handleInput, name }) => {
+
 	return (
 		<input
 			type='text'
 			value={name}
 			onChange={handleInput}
 			placeholder='Введите имя пользователя'
-			style={{ minWidth: '400px' }}
-		/>
+			style={{ minWidth: '400px' }} />
 	);
 };
 
